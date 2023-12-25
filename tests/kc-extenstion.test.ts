31d426b1762f30d2a34f134547704670d506d5f0
lequocthinh12 <quocthinhle2001@gmail.com>
2023-12-25 09:49:20 +0700

    feat: add more test cases

--- tests/kc-extenstion.test.ts
+++ tests/kc-extenstion.test.ts
@@ -64,12 +64,19 @@ describe('First-party native app authorize', () => {
             clientId: 'admin-cli'
         });
 
-        await keycloakAdminInstance.realms.create({realm: 'myrealm'});
+        await keycloakAdminInstance.realms.create({
+            realm: 'myrealm',
+            enabled: true,
+        });
+
         keycloakAdminInstance.setConfig({realmName: 'myrealm'});
+
 
         keycloakAdminInstance.clients.create({
             clientId: 'seenow',
             realm: 'myrealm',
+            directAccessGrantsEnabled: true,
+            publicClient: true,
         });
 
         keycloakAdminInstance.users.create({
@@ -130,9 +137,73 @@ describe('First-party native app authorize', () => {
 
         expect(response).toBeTruthy();
         expect(response.authorization_code).toBeTruthy();
+
+        // Exchange token
+        const exchangeTokenBody = {
+            code: response.authorization_code,
+            scope: 'openid profile',
+            client_id: 'seenow',
+            grant_type: 'authorization_code',
+            code_verifier: '6y1UBUBkwF769w7DfBvCQwasq57g4985x7YR8cFVkEzsdS_Q0IXZVXbSjEkqcU1BkNsSkT0sQGTN3-cyoGRG6dQVRinCPR0vo4gC_QsxkfNQ4v2CAU2x84UhEqQHK_Rq',
+        };
+
+        response = await makeFormUrcolondedRequest({
+            url: `http://${keycloakHost}:${keycloakPort}/realms/myrealm/protocol/openid-connect/token`,
+            body: new URLSearchParams(exchangeTokenBody),
+        });
+
+        expect(response).toBeTruthy();
+        expect(response.access_token).toBeTruthy();
+        expect(response.refresh_token).toBeTruthy();
     });
 
-    test('Login only', async () => {
+    test('Register but failed OTP more than 3 times', async () => {
+        let deviceSessionCode: any;
+
+        const body: any = {
+            client_id: 'seenow',
+            scope: 'openid profile',
+            phone_number: '0772283595',
+            code_challenge: '1el1nXSU92n7Oe7DX89mU_H9R8fksB4qnd8ge3sgM-4',
+            code_challenge_method: 'S256',
+            response_type: 'code',
+        };
+
+        let response = await makeFormUrcolondedRequest({
+            url: `http://${keycloakHost}:${keycloakPort}/realms/myrealm/first-party-native-app/authorize`,
+            body: new URLSearchParams(body),
+        });
+
+        expect(response).toBeTruthy();
+        expect(response.error).toBeTruthy();
+        expect(response.error).toEqual('otp_required');
+        expect(response.device_session).toBeTruthy();
+
+        for (let i = 0; i < 3; i += 1) {
+            deviceSessionCode = response.device_session;
+
+            body.device_session = deviceSessionCode;
+            body.otp = '123454';
+
+            response = await makeFormUrcolondedRequest({
+                url: `http://${keycloakHost}:${keycloakPort}/realms/myrealm/first-party-native-app/authorize`,
+                body: new URLSearchParams(body),
+            });
+
+            // The 3rd time enter a wrong otp must respond different
+            if (i == 2) {
+                expect(response).toBeTruthy();
+                expect(response.device_session).toBeTruthy();
+                expect(response.error).toEqual('session_cancelled');
+            } else {
+                expect(response).toBeTruthy();
+                expect(response.device_session).toBeTruthy();
+                expect(response.error).toEqual('otp_failed');
+            }
+        }
+    })
+
+    test('Login with valid credentials', async () => {
         let deviceSessionCode: any;
 
         const body: any = {
@@ -166,5 +237,60 @@ describe('First-party native app authorize', () => {
 
         expect(response).toBeTruthy();
         expect(response.authorization_code).toBeTruthy();
+
+        // Exchange token
+        const exchangeTokenBody = {
+            code: response.authorization_code,
+            scope: 'openid profile',
+            client_id: 'seenow',
+            grant_type: 'authorization_code',
+            code_verifier: '6y1UBUBkwF769w7DfBvCQwasq57g4985x7YR8cFVkEzsdS_Q0IXZVXbSjEkqcU1BkNsSkT0sQGTN3-cyoGRG6dQVRinCPR0vo4gC_QsxkfNQ4v2CAU2x84UhEqQHK_Rq',
+        };
+
+        response = await makeFormUrcolondedRequest({
+            url: `http://${keycloakHost}:${keycloakPort}/realms/myrealm/protocol/openid-connect/token`,
+            body: new URLSearchParams(exchangeTokenBody),
+        });
+
+        expect(response).toBeTruthy();
+        expect(response.access_token).toBeTruthy();
+        expect(response.refresh_token).toBeTruthy();
+    });
+
+    test('Login with invalid credentials', async () => {
+        let deviceSessionCode: any;
+
+        const body: any = {
+            client_id: 'seenow',
+            scope: 'openid profile',
+            phone_number: '0905083331',
+            code_challenge: '1el1nXSU92n7Oe7DX89mU_H9R8fksB4qnd8ge3sgM-4',
+            code_challenge_method: 'S256',
+            response_type: 'code',
+        };
+
+        let response = await makeFormUrcolondedRequest({
+            url: `http://${keycloakHost}:${keycloakPort}/realms/myrealm/first-party-native-app/authorize`,
+            body: new URLSearchParams(body),
+        });
+
+        expect(response).toBeTruthy();
+        expect(response.error).toBeTruthy();
+        expect(response.error).toEqual('passcode_required');
+        expect(response.device_session).toBeTruthy();
+
+        deviceSessionCode = response.device_session;
+
+        body.device_session = deviceSessionCode;
+        body.passcode = '100100';
+
+        response = await makeFormUrcolondedRequest({
+            url: `http://${keycloakHost}:${keycloakPort}/realms/myrealm/first-party-native-app/authorize`,
+            body: new URLSearchParams(body),
+        });
+
+        expect(response).toBeTruthy();
+        expect(response.error).toEqual('invalid_credentials');
+        expect(response.device_session).toEqual('');
     });
 });
